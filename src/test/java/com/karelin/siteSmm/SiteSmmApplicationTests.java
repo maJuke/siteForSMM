070b3ed253aaf87351bf195f73257e55529d01e0
maJuke <majukenson@gmail.com>
2024-01-13 18:07:16 +0300

    Start the project

---
+++ src/test/java/com/karelin/siteSmm/SiteSmmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karelin.siteSmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteSmmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
